46fdeab897c16232149d0342b6c5f8771bb14674
Parth Mehta <parth@geektrust.in>
2023-04-29 23:06:38 +0530

    moved crud handlers to crudHandlers package

---
+++ api/handlers/crudHandlers/listsCrudHandlers.js
@@ -0,0 +1,62 @@
+// load models
+const { List, Task } = require('../../DB/models');
+exports.getAllListsHandler = (req, res) => {
+    // return all the lists
+    List.find({}).then((lists) => {
+        res.send(lists);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+};
+
+exports.createListHandler = (req, res) => {
+    // create a new list and return the new list document with the id
+    let title = req.body.title;
+    let newList = new List({
+        title: title
+    });
+    newList.save().then((listDoc) => {
+        res.status(200).send(listDoc);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+};
+
+exports.updateListHandler = (req, res) => {
+    // updates a list
+    let id = req.params.id;
+    List.findOneAndUpdate({ _id: id }, {
+        $set: req.body
+    }).then((result) => {
+        res.sendStatus(200);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+};
+
+exports.deleteListHandler = (req, res) => {
+    // deletes a list
+    let id = req.params.id;
+    List.findOneAndDelete({ _id: id }).then((result) => {
+        if (result) {
+            res.status(200).send(result);
+        }
+        deleteTasksFromList(id);
+        res.status.apply(200).send('done');
+    }).catch((err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.status(500).send();
+    });
+};
+
+/* HELPER METHODS */
+let deleteTasksFromList = (listId) => {
+    console.log("inside");
+    Task.deleteMany({
+        _listId: listId
+    }).then(() => {
+        console.log("Tasks from " + _listId + " were deleted!");
+    });
+};
---
+++ api/handlers/crudHandlers/tasksCrudHandlers.js
@@ -0,0 +1,93 @@
+const { List, Task } = require('../../DB/models');
+
+exports.getAllTasksHandler = (req, res) => {
+    // return all the tasks of a list
+    let ListId = req.params.id;
+    Task.find({
+        _listId: ListId
+    }).then((record) => {
+        res.status(200).send(record);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+};
+
+exports.createNewTaskHandler = (req, res) => {
+    let listId = req.params.id;
+    List.find({
+        _id: listId
+    }).then((list) => {
+        if (list) {
+            return true;
+        };
+        return false;
+    }).then((canCreateTask) => {
+        if (canCreateTask) {
+            let task = new Task({
+                title: req.body.title,
+                _listId: listId
+            });
+            task.save().then((createdTask) => {
+                res.send(createdTask);
+            });
+        }
+        if (!canCreateTask) {
+            res.status(400).send("no list found with id: " + listId);
+        }
+    }).catch((err) => {
+        res.send(500).send(err);
+    });
+};
+
+exports.updateTaskHandler = (req, res) => {
+
+    List.findOne({
+        _id: req.params.listId,
+    }).then((list) => {
+        if (list) {
+            return true;
+        }
+        return false;
+    }).then((canUpdateTasks) => {
+        if (canUpdateTasks) {
+            Task.findOneAndUpdate({
+                _id: req.params.taskId,
+                _listId: req.params.listId
+            }, {
+                $set: req.body
+            }
+            ).then(() => {
+                res.send({ message: 'Updated successfully.' });
+            });
+        } else {
+            res.sendStatus(404);
+        }
+    });
+};
+
+exports.deleteTaskHandler = (req, res) => {
+    List.findOne({
+        _id: req.params.listId,
+        _userId: req.user_id
+    }).then((list) => {
+        if (list) {
+            // list object with the specified conditions was found
+            // therefore the currently authenticated user can make updates to tasks within this list
+            return true;
+        }
+        // else - the list object is undefined
+        return false;
+    }).then((canDeleteTasks) => {
+
+        if (canDeleteTasks) {
+            Task.findOneAndRemove({
+                _id: req.params.taskId,
+                _listId: req.params.listId
+            }).then((removedTaskDoc) => {
+                res.send(removedTaskDoc);
+            });
+        } else {
+            res.status(400).send("list not found with id:" + req.params.listId);
+        }
+    });
+};
